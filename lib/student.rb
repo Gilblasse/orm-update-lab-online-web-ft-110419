4d2025eba08912093139f3a2525734571787c684
Nethelbert <gilblasse@gmail.com>
2019-12-11 20:10:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,11 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  def initialize ("Tiffany", "11th")
+    @id = 
+    @name = 
+    @grade = 
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
