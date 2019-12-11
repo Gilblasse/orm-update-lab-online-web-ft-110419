e59344c2ddd2e0b081d71452dcc4d9a16ee31525
Nethelbert <gilblasse@gmail.com>
2019-12-11 20:28:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
     sql = <<-SQL
     INSERT INTO students(name,grade) VALUES (?,?)
     SQL
-    DB[:conn].execute(sql,self.name,self,grade)
+    DB[:conn].execute(sql,self.name,self.grade)
     binding.pry
     DB[:conn].execute("SELECT * FROM students")
   end
